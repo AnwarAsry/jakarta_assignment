65411ef828c451672cc58ee76b7dc32896857821
AnwarAsry <anwareducation03@gmail.com>
2026-03-10 23:36:48 +0100

    appuser service

---
+++ src/main/java/org/iths/jakartaassignment/Service/AppUserService.java
@@ -0,0 +1,31 @@
+package org.iths.jakartaassignment.Service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import org.iths.jakartaassignment.Model.AppUser;
+import org.iths.jakartaassignment.Repository.AppUserRepository;
+
+import java.util.List;
+
+@ApplicationScoped
+public class AppUserService {
+    private AppUserRepository appUserRepository;
+
+    public AppUserService() {
+    }
+
+    @Inject
+    public AppUserService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Transactional
+    public AppUser saveUser(AppUser user) {
+        return appUserRepository.save(user);
+    }
+
+    public List<AppUser> getAllUsers() {
+        return appUserRepository.findAll();
+    }
+}
